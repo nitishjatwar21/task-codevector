be93ff30663c288a5f26c04b5971834b73aa5ec8
Nitish Jatwar <nkjatwar@gmail.com>
2019-06-30 11:50:56 -0700

    Updates

---
+++ precache-manifest.4ed32f4271d2dff7697686e1f2ae33aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70adfa0783626204f16f9e5dd86647fe",
+    "url": "/task-codevector/index.html"
+  },
+  {
+    "revision": "83b2548b31d2a6895e0d",
+    "url": "/task-codevector/static/css/main.e0ff0436.chunk.css"
+  },
+  {
+    "revision": "cb3a7255acfc7e59bb9b",
+    "url": "/task-codevector/static/js/2.0d9a24f8.chunk.js"
+  },
+  {
+    "revision": "83b2548b31d2a6895e0d",
+    "url": "/task-codevector/static/js/main.0e7981a9.chunk.js"
+  },
+  {
+    "revision": "6116ae30a66bcfcddc89",
+    "url": "/task-codevector/static/js/runtime~main.928f0f79.js"
+  },
+  {
+    "revision": "2285db28fa844c2fdc68d6b9b7f41442",
+    "url": "/task-codevector/static/media/avenger.2285db28.jpg"
+  },
+  {
+    "revision": "9c97f4d6d3c1709694dcb6914847e260",
+    "url": "/task-codevector/static/media/jumanji.9c97f4d6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-codevector/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6725a7606bec79a528565755b3125341",
+    "url": "/task-codevector/static/media/mib.6725a760.jpg"
+  },
+  {
+    "revision": "e3d704fe896ad12a2ecc8e0a2d4579db",
+    "url": "/task-codevector/static/media/playstar.e3d704fe.jpg"
+  },
+  {
+    "revision": "7607f4784907890d607e2cce88f3e938",
+    "url": "/task-codevector/static/media/sanju.7607f478.jpg"
+  },
+  {
+    "revision": "b76dee06c16f7cd0f5829b336b3ad6dc",
+    "url": "/task-codevector/static/media/uri.b76dee06.jpg"
+  }
+]);
